6b3508c0faec8a30e2baac12be0a2c9812d8a186
Nathan <32364945+Strawkab@users.noreply.github.com>
2024-08-08 17:26:12 +0800

    Add files via upload
    implemented the bandit monster and a temporary fight mechanic
    TODO:
    implement initiative and a way to automatically make monsters choose actions

--- Dnd Character.py
+++ Dnd Character.py
@@ -9,46 +9,17 @@ def diceRoller(rolls, faces):
 	return total
 
 class Monster:
+	#monster info
 	alignment = None
 	monster_type = None
 	size = None
-
-	#ability scores
-	strength = 0
-	dexterity = 0
-	constitution = 0
-	intelligence = 0
-	wisdom = 0
-	charisma = 0
-
-	#ability modifiers
-	str_mod = (strength - 10) // 2
-	dex_mod = (dexterity - 10) // 2
-	con_mod = (constitution - 10) // 2
-	int_mod = (intelligence - 10) // 2
-	wis_mod = (wisdom - 10) // 2
-	cha_mod = (charisma - 10) // 2
-	   
-	#stats
-	armor_class = 10 + dex_mod
-	hit_die = 0
-	hit_points = hit_die + con_mod
+	combat_proficiencies = []
 	speed = 0
 	languages = []
 	challenge_rating = 0
 	exp_dropped = 0
-	proficiency_bonus = 0
-	combat_proficiencies = []
-		
-	#senses
-	passive_perception = 10 + wis_mod
+	hit_die = 0
 
-	def __init__(self, name):
-		#monster info
-		self.name = name
-		
-
-class Rat(Monsternster):
 	#ability scores
 	strength = 0
 	dexterity = 0
@@ -56,10 +27,79 @@ class Rat(Monsternster):
 	intelligence = 0
 	wisdom = 0
 	charisma = 0
+
 	
 
-rat = Rat("rat")
-print(rat.str_mod)
+	def __init__(self, name):
+		self.name = name
+
+		#ability modifiers
+		self.str_mod = (self.strength - 10) // 2
+		self.dex_mod = (self.dexterity - 10) // 2
+		self.con_mod = (self.constitution - 10) // 2
+		self.int_mod = (self.intelligence - 10) // 2
+		self.wis_mod = (self.wisdom - 10) // 2
+		self.cha_mod = (self.charisma - 10) // 2
+	   
+		#stats
+		self.armor_class = 10 + self.dex_mod
+		self.hit_points = self.hit_die + self.con_mod
+		
+		
+		
+		#senses
+		self.passive_perception = 10 + self.wis_mod
+		
+
+class Rat(Monster):
+	alignment = None
+	monster_type = None
+	size = None
+	combat_proficiencies = []
+	speed = 0
+	languages = []
+	challenge_rating = 0
+	exp_dropped = 0
+	hit_die = 4
+
+	#ability scores
+	strength = 2
+	dexterity = 11
+	constitution = 9
+	intelligence = 2
+	wisdom = 10
+	charisma = 4
+
+	#actions
+	def bite(self, target):
+		d20_result = diceRoller(1, 20)
+		if d20_result >= target.armor_class:
+			target.hit_points -= 1
+			print(str(d20_result) + ": " + self.name + " hit " + target.name + "for 1 dmg")
+
+		else:
+			print(str(d20_result) + ": " + self.name + " missed")
+
+
+	
+def fight(creature1, creature2):
+	while creature1.hit_points > 0 and creature2.hit_points > 0:
+		print(creature1.name + ": " + str(creature1.hit_points) + " " + creature2.name + ": " + str(creature2.hit_points))
+		creature1.bite(creature2)
+		creature2.bite(creature1)
+
+	if creature1.hit_points <= 0:
+		print(creature1.name + "has died")
+
+	if creature2.hit_points <= 0:
+		print(creature2.name + "has died")
+
+
+rat1 = Rat("ratatoile")
+rat2 = Rat("biggiecheese")
+
+fight(rat1, rat2)
+
 
 '''
 def characterCreation(name, character_race, character_class, age, strength, dexterity, constitution, intelligence, wisdom, charisma):
